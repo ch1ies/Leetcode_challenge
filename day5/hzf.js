0c6cf7f9f0d407c51d5161c221f453d13e3f4b10
flyfox11 <banjuanshutong@126.com>
2023-05-17 20:32:55 +0800

    fix: day5 refactor

--- day5/hzf.js
+++ day5/hzf.js
@@ -49,31 +49,60 @@ function theLongestStr(str) {
  * 最长回文子串 问题重构
  */
 function theLongestStr(str) {
+    // 判断是否是回文字符串，暂时不用
+    function isPalindrome(str) {
+        let len = str.length 
+        if(len < 2) return true 
+        let isOdd = len % 2 === 1
+        let start, end 
+        if(isOdd) {
+            let m = ~~(len / 2)
+            start = m - 1
+            end = m + 1
+        }else {
+            end = len / 2 
+            start = end - 1
+        }
+        while(start > -1) {
+            let sChar = str.charAt(start) 
+            let eChar = str.charAt(end) 
+            if(sChar !== eChar) {
+                return false 
+            }
+            start ++
+            end --
+        }
+        return true 
+    }
     if(typeof str !== 'string' || !str) {
         console.log('请输入有效字符串')
     }
-    // function isPalindrome(str) {
-    //     let len = str.length 
-    //     if(len < 2) return true 
-    //     let isOdd = len % 2 === 1
-    //     let start, end 
-    //     if(isOdd) {
-    //         let m = ~~(len / 2)
-    //         start = m - 1
-    //         end = m + 1
-    //     }else {
-    //         end = len / 2 
-    //         start = end - 1
-    //     }
-    //     while(start > -1) {
-    //         let sChar = str.charAt(start) 
-    //         let eChar = str.charAt(end) 
-    //         if(sChar !== eChar) {
-    //             return false 
-    //         }
-    //         start ++
-    //         end --
-    //     }
-    //     return true 
-    // }
+    let max = temp = ''
+    let len = str.length 
+    if(len < 2) return str 
+    function getPalindrome(str, l, r) {
+        while(l > 0 && r < len && str[l] === str[r]) {
+            l -- 
+            r ++;
+        }
+        if(str[l] === str[r]) {
+            let sLen = r - l + 1
+            let palindStr = str.substr(l, sLen)
+            return [sLen, palindStr]
+        }else if(str[l + 1] === str[r - 1]) {
+            let sLen = r - l - 1 
+            let palindStr = str.substr(l + 1, sLen) 
+            return [sLen, palindStr]
+        }else {
+            return [0]
+        }
+    }
+    for(let i = 0; i < len; i++) {
+        const [len1, str1] = getPalindrome(str, i, i) // 这个有必要，否则aaa aaaaa 这种情况判断不出来
+        const [len2, str2] = getPalindrome(str, i, i + 1)
+        if(!len1 && !len2) continue
+        temp = len1 > len2 ? str1 : str2 
+        max = max.length > temp.length ? max: temp
+    }
+    return max
 }
