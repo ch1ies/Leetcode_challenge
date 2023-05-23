d96173f2bbab37461a385fd0560d37a06814c414
flyfox11 <banjuanshutong@126.com>
2023-05-23 23:56:21 +0800

    feat: add day16 :tada:

--- algorithm/day16/hzf.js
+++ algorithm/day16/hzf.js
@@ -3,8 +3,45 @@
  * @param {Arrat} head 输入的数组
  * @returns {Array} 返回的数组
  */
-function reverseLink(head) {
-	
+function reverseLink(nums) {
+	function Node(val) {
+		this.val = val
+		this.next = null
+	}
+	function createLink(arr) {
+		let head = null, cur = null
+		for(let i = 0, len = arr.length; i < len; i++) {
+			let n = new Node(arr[i])
+			if(!head) {
+				cur = head = n
+			}else {
+				cur.next = n 
+				cur = n 
+			}
+		}
+		return head
+	}
+	function reverse(head) {
+    let prev = null, cur = head
+		while(cur) {
+			let temp = cur.next 
+			cur.next = prev
+			prev = cur
+			cur = temp
+		}
+		return prev
+	}
+	function linkToArr(head) {
+		let arr = [], cur = head
+		while(cur) {
+			arr.push(cur.val)
+			cur = cur.next
+		}
+		return arr
+	}
+	let link = createLink(nums)
+	let rLink = reverse(link)
+	return linkToArr(rLink)
 }
 
 
@@ -16,16 +53,18 @@ function reverseLink(head) {
 
 
 // 过程：
-// try 1.
+// try 1.linkToArr的方法里，忘记对cur = cur.next进行迭代，造成死循环，低级失误
+// try 2.成功
 
 // 伙伴评价
 
 
 // 感悟总结：
+// 此类问题多练手，然后尝试找不同解法
 
 /**
  * 翻转链表 重构
  */
-function reverseLink(head) {
+function reverseLink(nums) {
 	// 待实现
 }
