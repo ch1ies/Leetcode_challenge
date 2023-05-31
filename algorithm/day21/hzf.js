5f8356396bb037c288b7e851e23c9b0064a1e9af
flyfox11 <banjuanshutong@126.com>
2023-05-31 13:15:24 +0800

    fix: add

---
+++ algorithm/day21/hzf.js
@@ -0,0 +1,32 @@
+/**
+ * 编辑距离
+ * @param {string} word1 字符串1
+ * @param {string} word2 字符串2
+ * @returns {number} 返回最小操作数
+ */
+function minDistance(word1, word2) {
+	
+}
+
+
+
+// 输入：word1 = "horse", word2 = "ros" 输出：3
+// 输入：word1 = "intention", word2 = "execution" 输出：5
+
+
+// 过程：
+// try 1.
+
+
+// 伙伴评价
+
+
+// 感悟总结：
+
+
+/**
+ * 编辑距离
+ */
+function minDistance(word1, word2) {
+	// 待实现
+}
